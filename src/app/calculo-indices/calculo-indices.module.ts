4dec0d1b02464ca1c8bab1f75f9b87ef570f0275
franksotogithub <franksoto2012@gmail.com>
2020-07-30 11:11:33 -0500

    actualizando build

---
+++ src/app/calculo-indices/calculo-indices.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CalculoIndicesRoutingModule } from './calculo-indices-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CalculoIndicesRoutingModule
+  ]
+})
+export class CalculoIndicesModule { }
---
+++ src/app/tablas-maestras/tablas-maestras.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TablasMaestrasRoutingModule } from './tablas-maestras-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TablasMaestrasRoutingModule
+  ]
+})
+export class TablasMaestrasModule { }
