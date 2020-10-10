5520cc5c997d77aad1e5994e09444db43ff45eff
franksotogithub <franksoto2012@gmail.com>
2020-10-10 11:13:04 -0500

    mejorando la camara y el audio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   /*url_api: 'https://tranquil-hamlet-00282.herokuapp.com/',*/
   url_api: 'http://161.35.225.181:8000',
-  name_database_local:'ipc-local-database-prod-1'
+  name_database_local:'ipc-local-database-prod-2'
 };
