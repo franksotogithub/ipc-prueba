30650950b3467dccac360aaeadde7d5933d08f7a
franksotogithub <franksoto2012@gmail.com>
2021-01-27 18:12:15 -0500

    actualizando url del api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   /*url_api: 'https://tranquil-hamlet-00282.herokuapp.com/',*/
-  url_api: 'https://ipc.inei.gob.pe/',
+  url_api: 'https://ipc.inei.gob.pe',
   name_database_local:'ipc-local-database-prod-2'
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url_api: 'http://127.0.0.1:8000',
+  url_api: 'https://ipc.inei.gob.pe',
  /* url_api: 'http://161.35.225.181:8000',*/
   name_database_local:'ipc-local-database-2'
 };
